5f5c34a3c629444835ffc847d9cd8d8a5de8cc96
Mauro <mauro@osn.global>
2019-07-29 11:46:13 +0200

    Refactor DAO component and children to remove clutter

--- src/components/ContractGrid/index.js
+++ src/components/ContractGrid/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import { MDBRow } from "mdbreact";
-
 import ContractGridItem from './ContractGridItem'
 
-export default ({ contracts }) => (
-  <MDBRow className='pt-4 pb-4 border-bottom justify-content-center'>
-    <div className='col-12 text-md-center'>
-      <h3>Contracts</h3>
-    </div>
-    {contracts.map(contract => <ContractGridItem key={contract.address} contract={contract}/>)}
-  </MDBRow>
-)
+export default ({ contracts }) => {
+  if (contracts.length !== 0) {
+    return (
+      <div className='row pt-4 pb-4 border-bottom justify-content-center'>
+        <div className='col-12 text-md-center'>
+          <h3>Contracts</h3>
+        </div>
+        {contracts.map(contract => <ContractGridItem key={contract.address} contract={contract}/>)}
+      </div>
+    )
+  } else return null
+}
--- src/components/Dao/index.js
+++ src/components/Dao/index.js
@@ -6,6 +6,9 @@ import SocialIcons from '../SocialIcons'
 import TagGroup from '../Tags/TagGroup'
 import AuditList from '../AuditList'
 import DocumentGrid from '../DocumentGrid'
+import TextBlock from '../TextBlock'
+import LinkList from '../LinkList'
+import LinkBlock from '../LinkBlock'
 
 import daos from '../../data/daos'
 
@@ -40,67 +43,15 @@ export default ({ match }) => {
 				framework={dao.framework}
 				status={dao.status}
 			/>
-			{dao.assetsGoverned && (
-				<MDBRow>
-					<div className='col-12 pt-4 pb-4 border-bottom'>
-						<h3>Assets Governed</h3>
-						<p>{dao.assetsGoverned}</p>
-					</div>
-				</MDBRow>
-			)}
-			{dao.decentralizationLevel && (
-				<MDBRow>
-					<div className='col-12 pt-4 pb-4 border-bottom'>
-						<h3>Decentralization Level</h3>
-						<p>{dao.decentralizationLevel}</p>
-					</div>
-				</MDBRow>
-			)}
-			{dao.centralizationPoints && (
-				<MDBRow>
-					<div className='col-12 pt-4 pb-4 border-bottom'>
-						<h3>Centralization Points</h3>
-						<p>{dao.centralizationPoints}</p>
-					</div>
-				</MDBRow>
-			)}
-			{dao.contracts.length !== 0 && <ContractGrid contracts={dao.contracts} />}
-			<MDBRow>
-				{dao.contractProof && (
-		      <div className='col-12 pt-4 pb-4 border-bottom'>
-		        <h3>Contract Proof</h3>
-		        <a href={dao.contractProof} target='_blank' rel="noopener noreferrer" className='wrap-url'>{dao.contractProof}</a>
-		      </div>
-		    )}
-			</MDBRow>
-			{dao.audits.length !== 0 && <AuditList audits={dao.audits}/>}
-			{dao.documents.length!== 0 && <DocumentGrid documents={dao.documents}/>}
-			{dao.podcasts.length !== 0 && (
-				<MDBRow className='pt-4 pb-4 border-bottom'>
-					<h3 className='col-12'>Podcasts</h3>
-					<ul className='col-12 list-group'>
-						{dao.podcasts.map(podcast => (
-							<li className='list-group-item d-flex justify-content-md-between flex-column flex-md-row align-items-md-center'>
-								<a href={podcast.url} target='_blank' rel="noopener noreferrer" ><h6>{podcast.title}</h6></a>
-								<p className='ml-'>{podcast.date}</p>
-							</li>
-						))}
-					</ul>
-				</MDBRow>
-			)}
-			{dao.articles.length !== 0 && (
-				<MDBRow className='pt-4 pb-4 border-bottom'>
-					<h3 className='col-12'>Insightful Articles</h3>
-					<ul className='col-12 list-group'>
-						{dao.articles.map(article => (
-							<li className='list-group-item d-flex justify-content-md-between flex-column flex-md-row align-items-md-center'>
-								<a href={article.url} target='_blank' rel="noopener noreferrer" ><h6>{article.title}</h6></a>
-								<p className='ml-'>{article.date}</p>
-							</li>
-						))}
-					</ul>
-				</MDBRow>
-			)}
+			<TextBlock title='Assets Governed' text={dao.assetsGoverned} />
+			<TextBlock title='Decentralization Level' text={dao.decentralizationLevel} />
+			<TextBlock title='Centralization Points' text={dao.centralizationPoints} />
+			<ContractGrid contracts={dao.contracts} />
+			<LinkBlock title='Contract Proof' url={dao.contractProof} />
+			<AuditList audits={dao.audits}/>
+			<DocumentGrid documents={dao.documents}/>
+			<LinkList title='Podcasts' list={dao.podcasts} />
+			<LinkList title='Insightful Articles' list={dao.articles} />
 		</MDBContainer>
 	)
 }
