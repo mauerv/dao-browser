ab79089e6fe0cfccb0c1febc5e561eb6564075cb
Mauro <mauro@osn.global>
2019-08-04 16:58:09 +0200

    Implement basic CreateDao component

--- src/components/Admin/CreateDao/index.js
+++ src/components/Admin/CreateDao/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => (
-  <div>
-    <p>Create Dao</p>
-  </div>
-)
+class CreateDao extends Component {
+  render() {
+    return (
+     <div className='container'>
+       <div className='row pt-3 pb-3'>
+         <div className='col-12'>
+           <h2>Create DAO</h2>
+           <p>Add a new DAO to the registry.</p>
+         </div>
+       </div>
+       <div className='row pt-3 pb-3'>
+         <form>
+
+         </form>
+       </div>
+     </div>
+    )
+  }
+}
+
+export default CreateDao
