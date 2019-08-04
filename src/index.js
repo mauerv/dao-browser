05b321d48a31fbee11c9d9048ea401dab5b0b8bc
Mauro <mauro@osn.global>
2019-08-04 17:08:46 +0200

    Remove unused packages
    Bootstrap needs these packages for its .js, I'm not currently using those features.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import './styles/main.scss'
-import $ from 'jquery'
-import Popper from 'popper.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 import configureStore from './store/configureStore'
