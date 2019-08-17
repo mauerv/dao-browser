32df8e9874b065be6206f2901f8d3afbd59e67aa
Mauro <mauro@osn.global>
2019-08-17 15:27:41 +0200

    Fix bug with composit url on SocialIconList

--- src/components/SocialIcons/SocialIconItem.js
+++ src/components/SocialIcons/SocialIconItem.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const SocialIconItem = ({ network, id, extraUrl, theme }) => (
-  <a href={`${extraUrl}${id}`} target='__blank' rel="noopener noreferrer" className='mr-3 ml-3'>
-    <i className={`fab fa-${network} fa-2x footer-icon-${theme}`} aria-hidden="true"></i>
-  </a>
-)
+const SocialIconItem = ({ network, id, extraUrl, theme }) => {
+  const url = extraUrl ? `${extraUrl}${id}` : id
+  return (
+    <a href={url} target='__blank' rel="noopener noreferrer" className='mr-3 ml-3'>
+      <i className={`fab fa-${network} fa-2x footer-icon-${theme}`} aria-hidden="true"></i>
+    </a>
+  )
+
+}
 
 SocialIconItem.propTypes = {
   network: PropTypes.string.isRequired,
