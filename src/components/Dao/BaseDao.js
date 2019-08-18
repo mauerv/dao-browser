ac25fb03b8b943784b53d9a86d823c394a9a79d1
Mauro <mauro@osn.global>
2019-08-18 16:10:57 +0200

    Add Edit Button on DAO profiles for admins

--- src/components/Dao/BaseDao.js
+++ src/components/Dao/BaseDao.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { Link } from 'react-router-dom'
+
+import { useAuth0 } from '../../react-auth0-wrapper'
 
 import ContractGrid from '../ContractGrid'
 import SocialIcons from '../SocialIcons'
@@ -12,71 +15,82 @@ import LinkList from '../LinkList'
 import LinkBlock from '../LinkBlock'
 import ContributorGrid from '../ContributorGrid'
 
-class Dao extends Component {
-	componentDidMount() {
-		this.props.onFetchDao(this.props.match.params.id)
+import * as ROUTES from '../../constants/routes'
+
+const Dao = ({ onFetchDao, dao, match }) => {
+	useEffect(() => {
+		onFetchDao(match.params.id)
+		// eslint-disable-next-line
+	}, [])
+
+	const { isAuthenticated } = useAuth0()
+
+	if (!dao || !dao.contributors) {
+		return <div />
 	}
 
-	render() {
-		const { dao } = this.props
-
-		if (!dao || !dao.contributors) {
-			return <div />
-		}
-
-		return (
-			<MDBContainer>
-				<MDBRow className='text-center pt-4 pb-4 border-bottom'>
-					<MDBCol className='col-6 col-md-2 offset-3 offset-md-5'>
-						<img src={dao.image_url} alt='' className='img-thumbnail' />
-					</MDBCol>
-					<MDBCol className='col-12'>
-						<h1 className='mb-0'>{dao.name}</h1>
-						<a
-							href={dao.website}
-							target='_blank'
-							rel="noopener noreferrer"
-							className='break-words'
-						>
-							{dao.website}
-						</a>
-					</MDBCol>
-					<MDBCol className='col-12 mt-1'>
-						<p className='pt-3'>{dao.description}</p>
-					</MDBCol>
-					<MDBCol>
-						<SocialIcons
-							github={dao.github}
-							telegram={dao.telegram}
-							discord={dao.discord}
-							reddit={dao.reddit}
-							medium={dao.medium}
-							twitter={dao.twitter}
-							youtube={dao.youtube}
-							rocketchat={dao.rocketchat}
-							discourse={dao.discourse}
-						/>
-					</MDBCol>
-				</MDBRow>
-				<TagGroup
-					blockchain={dao.blockchain.name}
-					framework={dao.framework.name}
-					status={dao.status.name}
-				/>
-				{dao.contributors.length !== 0 && <ContributorGrid contributors={dao.contributors} />}
-				{dao.assets_governed && <TextBlock title='Assets Governed' text={dao.assets_governed} />}
-				{dao.decentralization_level && <TextBlock title='Decentralization Level' text={dao.decentralization_level} />}
-				{dao.centralization_points && <TextBlock title='Centralization Points' text={dao.centralization_points} />}
-				<ContractGrid contracts={dao.contracts} />
-				{dao.contract_proof && <LinkBlock title='Contract Proof' url={dao.contract_proof} />}
-				{dao.audits.length !== 0 && <AuditList audits={dao.audits}/>}
-				<DocumentGrid documents={dao.documents}/>
-				<LinkList title='Podcasts' list={dao.podcasts} />
-				<LinkList title='Insightful Articles' list={dao.articles} />
-				{dao.tags.length !== 0 && <TagGrid tags={dao.tags} />}
-			</MDBContainer>
-		)
-	}
+	return (
+		<MDBContainer>
+			<MDBRow>
+				{isAuthenticated && (
+					<Link
+						className='btn btn-primary'
+						to={`${ROUTES.ADMIN_DAOS}/${match.params.id}`}
+					>
+						Edit {dao.name}
+					</Link>
+				)}
+			</MDBRow>
+			<MDBRow className='text-center pt-4 pb-4 border-bottom'>
+				<MDBCol className='col-6 col-md-2 offset-3 offset-md-5'>
+					<img src={dao.image_url} alt='' className='img-thumbnail' />
+				</MDBCol>
+				<MDBCol className='col-12'>
+					<h1 className='mb-0'>{dao.name}</h1>
+					<a
+						href={dao.website}
+						target='_blank'
+						rel="noopener noreferrer"
+						className='break-words'
+					>
+						{dao.website}
+					</a>
+				</MDBCol>
+				<MDBCol className='col-12 mt-1'>
+					<p className='pt-3'>{dao.description}</p>
+				</MDBCol>
+				<MDBCol>
+					<SocialIcons
+						github={dao.github}
+						telegram={dao.telegram}
+						discord={dao.discord}
+						reddit={dao.reddit}
+						medium={dao.medium}
+						twitter={dao.twitter}
+						youtube={dao.youtube}
+						rocketchat={dao.rocketchat}
+						discourse={dao.discourse}
+					/>
+				</MDBCol>
+			</MDBRow>
+			<TagGroup
+				blockchain={dao.blockchain.name}
+				framework={dao.framework.name}
+				status={dao.status.name}
+			/>
+			{dao.contributors.length !== 0 && <ContributorGrid contributors={dao.contributors} />}
+			{dao.assets_governed && <TextBlock title='Assets Governed' text={dao.assets_governed} />}
+			{dao.decentralization_level && <TextBlock title='Decentralization Level' text={dao.decentralization_level} />}
+			{dao.centralization_points && <TextBlock title='Centralization Points' text={dao.centralization_points} />}
+			<ContractGrid contracts={dao.contracts} />
+			{dao.contract_proof && <LinkBlock title='Contract Proof' url={dao.contract_proof} />}
+			{dao.audits.length !== 0 && <AuditList audits={dao.audits}/>}
+			<DocumentGrid documents={dao.documents}/>
+			<LinkList title='Podcasts' list={dao.podcasts} />
+			<LinkList title='Insightful Articles' list={dao.articles} />
+			{dao.tags.length !== 0 && <TagGrid tags={dao.tags} />}
+		</MDBContainer>
+	)
 }
 
 export default Dao
